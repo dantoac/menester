907f1596670f8c02390d3a665e4e638e0fc3de75
DantoAC <daniel@nim.io>
2012-10-22 00:11:00 -0300

    missed :P

--- controllers/comment.py
+++ controllers/comment.py
@@ -1,26 +1,30 @@
 # coding: utf8
 
-
-
-
-def task():
+def new():
     # comentar una tarea
-    task_uuid = request.vars.tuuid
+    uuid = request.vars.uuid
     
-    if task_uuid:
-        db.comment.object_uuid.default = task_uuid
+    if uuid:
+        db.comment.target_uuid.default = uuid
         db.comment.author.default = db.auth_user[auth.user_id].email
 
     form = crud.create(db.comment)
 
-    query = (db.comment.object_uuid.contains(task_uuid))
+    query = (db.comment.target_uuid == uuid)
 
-    tasklist_dataset = db(query).select(db.comment.body,
-                                        db.comment.author,
-                                        db.comment.created_on,
-                                        db.comment.id,
-                                        orderby=~db.comment.id)
+    dataset = db(query).select(db.comment.body,
+                               db.comment.author,
+                               db.comment.created_on,
+                               db.comment.id,
+                               orderby=~db.comment.id)
     
-    return dict(form=form, comments=tasklist_dataset)
+    return dict(form=form, comments=dataset)
 
 
+def _delete():
+    comment_id = request.args(0)
+    delete = db(db.comment.id == comment_id).delete()
+    if delete:
+        response.flash = 'Comentario eliminado'
+    else:
+        response.flash = 'omgwtf'
