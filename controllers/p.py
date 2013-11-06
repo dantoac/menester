23170e27a07b3b63cdaf54057b1a479c6da4c2ce
Daniel Aguayo Catalán <daniel@nim.io>
2013-11-06 13:16:53 -0300

    ADD representación alternativa de proyectos en p/aoeu

--- controllers/p.py
+++ controllers/p.py
@@ -6,6 +6,79 @@ def index():
     return dict()
 
 @auth.requires_login()
+def aoeu():
+            
+    dataset = db.project
+
+    open_task_dataset = db((db.task.project_uuid==db.project.uuid)
+                           & (db.task.closed==False)
+                       ).select(db.project.uuid, 
+                                db.task.id.count(), 
+                                groupby=db.project.uuid)
+    
+    open_task = Storage({t.project.uuid:t['COUNT(task.id)'] for t in open_task_dataset})
+
+
+    db.project.name.represent = lambda name,r: CAT(A(r.name, 
+                                                     _href=URL(args=['edit','project',r.id], user_signature=True),
+                                                     _class='btn btn-large btn-block {0}'.format('btn-success' if r.close else 'btn-danger')),
+
+                                                   DIV(
+                                                       A(TAG.i(_class='icon icon-arrow-down'),
+                                                                   T('Ingresos'),
+                                                                   _href= URL(c='i',f='index',vars={'p':r.uuid}),
+                                                                   _class='btn'),
+                                                       A(TAG.i(_class='icon icon-arrow-up'),
+                                                                   T('Egresos'),
+                                                                   _href= URL(c='e',f='index',vars={'p':r.uuid}),
+                                                                   _class='btn'),
+                                                       A(TAG.i(_class='icon icon-tasks'),
+                                                                   T('Tareas'),
+                                                                   _href= URL(c='t',f='index',vars={'p':r.uuid}),
+                                                                   _class='btn'),
+                                                       _class='btn-group'),
+                                                   DIV(
+                                                       DIV('Comienza: ', prettydate(r.start)),
+                                                       DIV('Finaliza: ', prettydate(r.end)),
+                                                       _class='well well-small'
+                                                   )
+                                               )
+
+    db.project.start.readable = False
+    db.project.end.readable = False
+    db.project.close.readable = False
+
+
+    grid = SQLFORM.grid(dataset, orderby=db.project.close|~db.project.created_on|~db.project.modified_on,
+                        fields = [db.project.name,
+                                  db.project.uuid,
+                                  db.project.start,
+                                  db.project.end,
+                                  db.project.close,
+                                  ], 
+                        details=False, deletable=True, editable=True,
+                        paginate = 10,
+                        links = [
+                            lambda r: XML('''
+                            <div id="project-chart-%(id)s"></div>
+                            <script>
+                            new Morris.Line({
+                            element: 'project-chart-%(id)s',
+                            data: %(data)s,
+                            xkey: 'y',
+                            ykeys: ['a','b','c'],
+                            labels: ['Ingresos','Egresos','Utilidades'],
+                            lineColors: ['#007bcc','#d64b46','#4e9a06']
+                            });
+                            </script>
+                            ''' % {'id':r.id, 'data':flux(r.uuid)})
+                        ]
+    )
+   
+    return {'projects': grid}
+
+
+@auth.requires_login()
 def list():
         
     #query proyectos en cualquier estado
