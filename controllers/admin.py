f751d7f53d2601cc0cc39ab8d6f6fa95b6f7a116
Daniel Aguayo <daniel@nim.io>
2014-04-03 16:54:47 -0300

    MOd permite activar usuarios sin necesidad de entrar a appadmin

--- controllers/admin.py
+++ controllers/admin.py
@@ -5,6 +5,9 @@ def index():
     if not auth.user_id == 1: redirect(URL(f='error'))
     menu = UL([A(t, _href=URL(args=t)) for t in db.tables])
 
+
+    db.auth_user.registration_key.writable = True
+
     admin = SQLFORM.smartgrid(db[request.args(0) or 'auth_user'])
 
     return {'menu': menu,'admin': admin}
